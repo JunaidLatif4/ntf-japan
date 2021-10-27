513b08da7ef73005c1882f3dfcaffdc4a2156fc3
Junaid Latif <junaidlatif004@gmail.com>
2021-10-27 19:13:36 +0500

    Section3 Added

--- src/Component/Home/Components/Section2.jsx
+++ src/Component/Home/Components/Section2.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Blood from "../../../assets/blood.png"
+import C1 from "../../../assets/c1.png"
+
 import "./Section2.scss"
 
 const Section2 = () => {
@@ -11,14 +14,67 @@ const Section2 = () => {
                         <div className="heading">
                             The 8 Bushido Codes
                         </div>
-                        <div className="detai">
+                        <div className="detail">
                             Bushidō (武士道, "The way of the warrior") is a moral code relating to 侍 (Samurai) attitudes, behaviour and lifestyle, where all Samurais live and die by.
+                            <br />
                             <br />
                             These 8 virtues of the Bushido code are each embodied by the 8 legendary Shoguns (将軍, shōgun):
                         </div>
                     </div>
-
+                    <div className="data">
+                        <div className="cards">
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                            <div className="card">
+                                <img src={C1} alt="ERROR" />
+                                <div className="h"> Gi </div>
+                                <div className="s"> "Shogun of Justice" </div>
+                                <div className="d"> Samurais make a full commitment to their decisions. </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <img className="blood" src={Blood} alt="ERROR" />
             </section>
         </>
     )
